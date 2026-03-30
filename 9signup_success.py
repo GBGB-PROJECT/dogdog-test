38277d9cb76cf7841304e323d37f85003f4f8364
junhwan <junhwan1310@gmail.com>
2026-03-30 12:21:16 +0900

    연습 feat: 회원가입 완료 화면

---
+++ 9signup_success.py
@@ -0,0 +1,45 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "회원가입 완료"
+    # page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    # page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    
+    page.add(
+        ft.Container(
+            expand=True, # ☑️ 이게 없으면 배경이 상단만 적용됨 
+            width=float("inf"), # ☑️ 이게 없으면 배경이 왼쪽만 적용됨
+
+            # 🟩 수정: gradient 제거하고 단색 배경으로 변경
+            bgcolor=ft.Colors.YELLOW,
+
+            content=ft.Column(
+                expand = True,
+                alignment=ft.MainAxisAlignment.CENTER, # ☑️ 이게 없으면 로고가 천장에 붙음
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER, # ☑️ 이게 없으면 로고가 왼쪽에 붙음
+                controls=[
+                    ft.Image(
+                        src="checkone.png",
+                        width=150,
+                        height=150,
+                    ),
+                    ft.Text("회원 가입이 완료되었습니다.", weight=ft.FontWeight.W_800, color=ft.Colors.BLACK, size=20)
+                ],
+            ),
+        )
+    )
+
+if __name__ == "__main__":
+    import webbrowser
+    import os
+
+    if os.getenv("FLET_NO_BROWSER"):
+        webbrowser.open = lambda *args, **kwargs: None
+
+    ft.app(
+        main,
+        assets_dir="assets",
+        view=ft.AppView.WEB_BROWSER,
+        port=34636,
+    )
